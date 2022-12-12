b0161c7a2a3e7a9679c8da110e6cbb9c58cc5a79
takumi-1016 <tn1016recruit@gmail.com>
2022-12-12 18:35:16 +0900

    チュートリアル5

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/folders/{id}/tasks', [\App\Http\Controllers\TaskController::class, 'index'])->name('tasks.index');
+
+Route::get('/folders/create', [\App\Http\Controllers\FolderController::class, 'showCreateForm'])->name('folders.create');
+Route::post('/folders/create', [\App\Http\Controllers\FolderController::class, 'create']);
