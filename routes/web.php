e71f0fae28b8a551be2b904cc09d9d867e147554
takumi-1016 <tn1016recruit@gmail.com>
2022-12-12 11:03:31 +0900

    チュートリアル3

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+Route::get('/folders/{id}/tasks', [\App\Http\Controllers\TaskController::class, 'index'])->name('tasks.index');
