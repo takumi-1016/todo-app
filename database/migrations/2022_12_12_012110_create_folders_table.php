47ace8449947def6eb1b18587aaf8dec0286049d
takumi-1016 <tn1016recruit@gmail.com>
2022-12-12 16:28:06 +0900

    チュートリアル４

--- database/migrations/2022_12_12_012110_create_folders_table.php
+++ database/migrations/2022_12_12_012110_create_folders_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('folders', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('title', 20);
             $table->timestamps();
         });
