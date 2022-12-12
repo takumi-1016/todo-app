543231b4ec177a28f61dea79e1e4265dcb90d4ac
takumi-1016 <tn1016recruit@gmail.com>
2022-12-12 19:49:47 +0900

    チュートリアル7

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ Route::post('/folders/create', [\App\Http\Controllers\FolderController::class, '
 
 Route::get('/folders/{id}/tasks/create', [\App\Http\Controllers\TaskController::class, 'showCreateForm'])->name('tasks.create');
 Route::post('/folders/{id}/tasks/create', [\App\Http\Controllers\TaskController::class, 'create']);
+
+Route::get('/folders/{id}/tasks/{task_id}/edit', [\App\Http\Controllers\TaskController::class, 'showEditForm'])->name('tasks.edit');
+Route::post('/folders/{id}/tasks/{task_id}/edit', [\App\Http\Controllers\TaskController::class, 'edit']);
